34b417e292c1a6d79af768c95f015c593651d82f
Jacoeb <officialjacoeb@gmail.com>
2022-09-29 01:44:04 +0800

    remove staticfiles storage

--- blap/settings.py
+++ blap/settings.py
@@ -132,4 +132,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
