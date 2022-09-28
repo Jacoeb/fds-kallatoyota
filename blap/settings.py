ff6a73d6c67ccb76bd7ed6aea3b85515f7600d26
Jacoeb <officialjacoeb@gmail.com>
2022-09-29 01:39:47 +0800

    ganti kembali settings

--- blap/settings.py
+++ blap/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/invoice')
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR, 'static/invoice']
