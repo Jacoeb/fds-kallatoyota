58a25b14cf5e0d35a211524b8a6cd81e2eea9a9e
Jacoeb <officialjacoeb@gmail.com>
2022-09-29 10:10:23 +0800

    setting main.html

--- blap/settings.py
+++ blap/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [BASE_DIR, 'static/invoice']
+STATICFILES_DIRS = [BASE_DIR, '/static/']
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
