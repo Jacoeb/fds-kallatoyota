d982831f624d9bfcda92f145c93751adb74e5d52
Jacoeb <officialjacoeb@gmail.com>
2022-09-29 11:42:22 +0800

    perbaikan staticfiles

--- blap/settings.py
+++ blap/settings.py
@@ -120,13 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
-]
 
-MEDIA_ROOT = MEDIA_DIR
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_ROOT = MEDIA_DIR
 
 
 # Default primary key field type
