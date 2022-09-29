1e357becd4d3d1ccd3103aa3cdaa1e22864e73f2
Jacoeb <officialjacoeb@gmail.com>
2022-09-29 11:15:22 +0800

    commit static 1

--- blap/settings.py
+++ blap/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [BASE_DIR, '/static/invoice/']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static")
+]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
